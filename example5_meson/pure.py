7ed3b6e9fc2ae1473a72dd83e2ca66bd97cbd134
Leah Wasser <leah@pyopensci.org>
2023-01-26 15:46:38 -0700

    Add: meson

---
+++ example5_meson/pure.py
@@ -0,0 +1,25 @@
+def foo():
+    return 'bar'
+
+"""
+More on this module here
+"""
+
+
+def add_values(num1, num2):
+    """A function that adds two integer values. 
+    
+    Parameters
+    ----------
+    num1 : int
+        mmm
+    num2 : int
+        mmm
+    
+    Returns
+    -------
+    value : int
+        num 1 and num 2 added together
+    
+    """
+    return num1 + num2
