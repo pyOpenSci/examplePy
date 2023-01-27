214afa8963d6af04fcd72ed78aa4f6e14a172033
Leah Wasser <leah@pyopensci.org>
2023-01-26 17:22:36 -0700

    Fix: Cleanup merge conflicts - time to take a break

--- example6_pdm_meson/examplePy/__init__.py
+++ example6_pdm_meson/examplePy/__init__.py
@@ -1,5 +1,4 @@
 '''
-<<<<<<< HEAD
 The __init__.py file is required to import the directory as a package.
 It can start as an empty file.
 
@@ -7,12 +6,4 @@ It can start as an empty file.
 
 from .module1 import add_values
 
-=======
-The __init__.py file is required to import the directory as a package, and 
-can start empty.
-
-It is used for....
-'''
-
->>>>>>> upstream/main
 
